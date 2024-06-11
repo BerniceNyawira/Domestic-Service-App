d7fa603d6615aa6b47dc8d375d915037504ceecb
BerniceNyawira <bernice.githinji@strathmore.edu>
2024-06-11 17:00:04 +0300

    Scratch role-based access for now :)

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,9 +21,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-
-        //other middleware
-        'role' => \App\Http\Middleware\RoleMiddleware::class,
     ];
 
     /**
